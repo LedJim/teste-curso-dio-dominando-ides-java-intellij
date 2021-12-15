048f12d19126c7530b7063f44c3307c6d36a259c
LedJim <raulrojas.totvs@gmail.com>
2021-12-15 00:40:35 -0300

    Primer Commit Carajo

---
+++ src/br/com/dio/PrimeroProgramaJava.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeroProgramaJava {
+    public static void main(String[] args) {
+        // deseo crear mi Clase Gato aquí
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+        System.out.println(gato);
+
+//        int num1 = 10;
+//        int num2 = 3;
+//        System.out.println("Hola mis Carnales: esto da = " + (num1+num2));
+    }
+}
